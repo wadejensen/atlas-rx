98433783983e1964669045efd3e411ec096ff09f
“Wade <hello@wade-jensen.com>
2019-08-30 06:44:02 +1000

    cleanup

--- app/web/src/main/ts/app.ts
+++ app/web/src/main/ts/app.ts
@@ -33,25 +33,6 @@ googlePlacesAutocomplete("redfe").then( json => console.log(json));
 setupStateChangeListeners();
 setupContentUpdateListeners();
 
-// const listingsRequest = new ListingsRequest({
-//     boundingBox: Geo.boundingBox(
-//       new Coord(-33.87755059238735,151.01137831057133),
-//       new Coord(-33.98294912171756,151.29255964602055),
-//     ),
-//     roomType: RoomType.PRIVATE_ROOM,
-//     minBudget: 300,
-//     maxBudget: 350,
-// });
-
-// fetch(window.location + "flatmates/listings", {
-//     method: "POST",
-//     headers: {
-//         "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(listingsRequest),
-// })
-//   .then(resp => resp.json())
-//   .then(json => console.log(json));
 
 fetch(window.location + "google/places-autocomplete/2 George St", {
     method: "GET",
@@ -77,10 +58,10 @@ fetch(window.location + "google/places-autocomplete/2 George St", {
 // centre google map on lat lng of suggestion. zoom level = 15 -> tick
 // user can now click on results to see travel time -> TODO
 
-// user clicks filters menu -> TODO
-// user enters search criteria -> TODO
-// user hits search now button -> TODO
-// we update the standing filters state of the users flatmates requests -> TODO
+// user clicks filters menu -> tick
+// user enters search criteria -> tick
+// user hits search now button -> tick
+// we update the standing filters state of the users flatmates requests -> tick
 // user clicks on result -> TODO
 // user gets a flyout of the result with details, including travel time -> TODO
 
@@ -90,4 +71,4 @@ fetch(window.location + "google/places-autocomplete/2 George St", {
 
 // a better, safer throttle -> tick
 
-// need to remove duplicate map markers before plotting more
+// need to remove duplicate map markers before plotting more -> tick
