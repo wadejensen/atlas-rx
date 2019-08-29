ad99fd3b0cef0cf3044d81bd99612dfa18041399
“Wade <hello@wade-jensen.com>
2019-08-30 06:43:18 +1000

    remove chart

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -17,27 +17,3 @@ function initMap() {
         streetViewControl: false,
     });
 }
-
-function initChart() {
-    /** accesses global declared in index.html */
-    var ctx = document.getElementById('chart').getContext('2d');
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
-        // The data for our dataset
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45]
-            }]
-        },
-        // Configuration options go here
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-        }
-    });
-}
